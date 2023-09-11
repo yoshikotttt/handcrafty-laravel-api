68f0192923b4c721cbe23818b2dd9b0152591e82
yoshikotttt <myqic.n@gmail.com>
2023-09-11 14:36:11 +0900

    画像保存　確認済み

--- app/Models/Items.php
+++ app/Models/Items.php
@@ -8,12 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Items extends Model
 {
     // protected $fillable = ['title', 'description'];
-    protected $fillable = ['user_id', 'title', 'category_id','description', 'status', 'production_time_per_minutes', 'reference_url', 'memo', 'show_memo'];
+    protected $fillable = ['user_id', 'title', 'category_id','description', 'status', 'production_time_per_minutes', 'reference_url','image_url', 'memo', 'show_memo'];
 
     // Category モデルとのリレーションを設定
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+        public function images()
+    {
+        return $this->hasMany(Image::class);
     }
 }
 
